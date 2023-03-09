02f7d3cd59afa902fe899af93b7f02d4396f4ca0
Aleksey Kosin <akosin@udm.ru>
2023-03-09 18:28:16 +0400

    meeting bugs

--- bookclub/bookclub/settings.py
+++ bookclub/bookclub/settings.py
@@ -29,7 +29,7 @@ load_dotenv(dotenv_path=env_path)
 SECRET_KEY = os.getenv('SECRET_KEY', 'workaround')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = bool(os.getenv('DEBUG', True))
 00
 ALLOWED_HOSTS = ['*']
 
