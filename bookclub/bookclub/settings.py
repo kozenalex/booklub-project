8983749034d0dec8f30649911f24c1fd67ea12d9
Aleksey Kosin <akosin@udm.ru>
2023-03-20 17:07:06 +0400

    settings error

--- bookclub/bookclub/settings.py
+++ bookclub/bookclub/settings.py
@@ -142,8 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = ['/opt/render/project/src/bookclub/staticfiles/']
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # media files
